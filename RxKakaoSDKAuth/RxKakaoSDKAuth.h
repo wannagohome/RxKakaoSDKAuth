f8f84a86e5a3107289aca32f042c17e8b62276bc
wannagohome <iwwuf7@gmail.com>
2020-09-24 16:36:34 +0900

    Initial Commit

---
+++ RxKakaoSDKAuth/RxKakaoSDKAuth.h
@@ -0,0 +1,19 @@
+//
+//  RxKakaoSDKAuth.h
+//  RxKakaoSDKAuth
+//
+//  Created by jinho jang on 2020/09/24.
+//  Copyright © 2020 People. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxKakaoSDKAuth.
+FOUNDATION_EXPORT double RxKakaoSDKAuthVersionNumber;
+
+//! Project version string for RxKakaoSDKAuth.
+FOUNDATION_EXPORT const unsigned char RxKakaoSDKAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxKakaoSDKAuth/PublicHeader.h>
+
+
